940ec83ecd4d40452e78972bc91b6cfcefc8e99d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-12-12 09:47:28 +0000

    day 11 working

---
+++ day11/src/part1.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+use std::io::{BufReader, prelude::*};
+use std::collections::HashMap;
+
+fn solve(filename: &str) -> usize{
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut machines = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let mut parts = line.split(": ");
+        let machine = String::from(parts.next().unwrap());
+        let outputs = parts.next().unwrap().split(" ").map(String::from).collect::<Vec<_>>();
+        machines.insert(machine, outputs);
+    }
+
+    let mut paths = vec![vec!["you"]];
+    let mut valid_paths = vec![];
+
+    loop {
+        let mut new_paths = vec![];
+
+        for p in paths {
+            for next in &machines[p[p.len() - 1]] {
+                let mut new_p = p.clone();
+                new_p.push(next);
+                if next == "out" {
+                    println!("{new_p:?}");
+                    valid_paths.push(new_p);
+                } else {
+                    new_paths.push(new_p);
+                }
+            }
+        }
+
+        if new_paths.len() == 0 {
+            break;
+        }
+        paths = new_paths;
+    }
+
+    valid_paths.len()
+}
+
+fn main() {
+    println!("{}", solve("input"));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn day11_part1_test() {
+        let solution = solve("test_input");
+        assert_eq!(solution, 5);
+    }
+}
